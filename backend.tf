27fce50f54bb1568eb092462677218074f6fdc77
Priyanka Nade <143316400+Priyanka-Nade2000@users.noreply.github.com>
2024-10-13 21:39:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-devproject-terraform-nade112"
+    bucket = "my-devproject-terraform-nade17pro"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table1"
